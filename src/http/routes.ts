176886c4df7bcd6a50f915807ae23bf88cf898a3
mgckaled <marcelgckaled@gmail.com>
2023-09-25 12:15:04 -0300

    feat: add authenticate's controllers and tests

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,7 +1,10 @@
 import { FastifyInstance } from 'fastify'
 
-import { register } from './controllers/register'
+import { authenticate } from '@/http/controllers/authenticate'
+import { register } from '@/http/controllers/register'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
+
+  app.post('/sessions', authenticate)
 }
