769447f71a74337f543c3e5218bce207c6c1bd12
mgckaled <marcelgckaled@gmail.com>
2023-10-03 10:40:10 -0300

    test: define authenticate 2e2 test

--- src/http/controllers/authenticate.spec.ts
+++ src/http/controllers/authenticate.spec.ts
@@ -1,5 +1,31 @@
-import { test } from 'vitest'
+import { app } from '@/app'
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
-test('ok', () => {
-  return console.log('ok')
+describe('Authenticate (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to authenticate', async () => {
+    await request(app.server).post('/users').send({
+      name: 'John Doe2',
+      email: 'johndoe2@example.com',
+      password: '1234567',
+    })
+
+    const response = await request(app.server).post('/sessions').send({
+      email: 'johndoe2@example.com',
+      password: '1234567',
+    })
+
+    expect(response.status).toEqual(200)
+    expect(response.body).toEqual({
+      token: expect.any(String),
+    })
+  })
 })
--- src/http/controllers/register.spec.ts
+++ src/http/controllers/register.spec.ts
@@ -13,9 +13,9 @@ describe('Register (e2e)', () => {
 
   it('should be able to register', async () => {
     const response = await request(app.server).post('/users').send({
-      name: 'John Doe',
-      email: 'johndoe@example.com',
-      password: '123456',
+      name: 'John dae',
+      email: 'johndae@example.com',
+      password: '654321',
     })
 
     expect(response.statusCode).toEqual(201)
