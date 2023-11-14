8fffa49c18e5aba170fa2027a40d3bc3eb5a2562
mgckaled <marcelgckaled@gmail.com>
2023-11-14 13:29:03 -0300

    feat: create refresh controller and its test

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -2,6 +2,7 @@ import { FastifyInstance } from 'fastify'
 
 import { authenticate } from '@/http/controllers/users/authenticate'
 import { profile } from '@/http/controllers/users/profile'
+import { refresh } from '@/http/controllers/users/refresh'
 import { register } from '@/http/controllers/users/register'
 import { verifyJwt } from '@/http/middlewares/verify-jwt'
 
@@ -9,6 +10,8 @@ export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', register)
 
   app.post('/sessions', authenticate)
+
+  app.patch('/token/refresh', refresh)
 
   /** Authenticated */
   app.get('/me', { onRequest: [verifyJwt] }, profile)
